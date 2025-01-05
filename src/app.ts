4d800829f44e4b815f5a387974722e9f5b444647
mike dupont <mike.dupont@introspector.local>
2025-01-05 08:36:28 -0500

    first version of app working with eliza chat client
    returns work in progress.
    
    run with `pnpm dev`
    ```
    [TypeScript] 8:36:05 AM - Found 0 errors. Watching for file changes.
    [App] Available routes:
    [App]
    [App] ╔═══════════╤═══════════════════╗
    [App] ║ Method(s) │ Path              ║
    [App] ╟───────────┼───────────────────╢
    [App] ║      POST │ /:agentId/message ║
    [App] ╚═══════════╧═══════════════════╝
    [App]
    [App] [08:36:05.582] INFO (909127): Server listening at http://127.0.0.1:3000
    [App] [08:36:05.582] INFO (909127): Server listening at http://192.168.1.90:3000
    [App] [08:36:05.582] INFO (909127): Server listening at http://192.168.68.64:3000
    [App] [08:36:05.582] INFO (909127): Server listening at http://10.42.0.0:3000
    [App] [08:36:05.582] INFO (909127): Server listening at http://172.17.0.1:3000
    [App] [08:36:08.288] INFO (909127): incoming request
    [App]     reqId: "req-1"
    [App]     req: {
    [App]       "method": "POST",
    [App]       "url": "/TINE-IntrospectorIsNotEliza/message",
    [App]       "host": "localhost:3000",
    [App]       "remoteAddress": "127.0.0.1",
    [App]       "remotePort": 47144
    [App]     }
    [App] [08:36:08.291] INFO (909127): request completed
    [App]     reqId: "req-1"
    [App]     res: {
    [App]       "statusCode": 200
    [App]     }
    [App]     responseTime: 2.3721660375595093
    ```

--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,12 @@
-import * as path from 'node:path';
-import AutoLoad, {AutoloadPluginOptions} from '@fastify/autoload';
 import { FastifyPluginAsync } from 'fastify';
-import { fileURLToPath } from 'node:url'
+import fastifyPrintRoutes from 'fastify-print-routes'
+import directClient from './routes/root.js';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+class AppOptions{}
 
-export type AppOptions = {
-  // Place your custom options for app below here.
-} & Partial<AutoloadPluginOptions>;
-
-
-// Pass --options via CLI arguments in command to enable these options.
-const options: AppOptions = {
-}
-
-const app: FastifyPluginAsync<AppOptions> = async (
-    fastify,
-    opts
-): Promise<void> => {
-  // Place here your custom code!
-
-  // Do not touch the following lines
-
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  void fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: opts,
-    forceESM: true
-  })
-
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  void fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: opts,
-    forceESM: true
-  })
-
+const app: FastifyPluginAsync<AppOptions> = async (fastify, opts): Promise<void> => {
+  void fastify.register(fastifyPrintRoutes)
+  void fastify.register(directClient)
 };
 
 export default app;
-export { app, options }
